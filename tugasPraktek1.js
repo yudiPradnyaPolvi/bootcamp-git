76f7eee884009222ea1f7f6e4be1ee122d9a925d
yudiPradnyaPolvi <pradnyakemenuh@gmail.com>
2023-02-28 21:57:43 +0800

    inisiasi project

---
+++ tugasPraktek1.js
@@ -0,0 +1,61 @@
+console.log ("5 varibael untuk menghitung 3 angka")
+let angka1=10
+let angka2=10
+let angka3=10
+
+let penjumlahanAngka = angka1+angka2+angka3
+let penguranganAngka = angka1-angka2-angka3
+let perkalianAngka = angka1*angka2*angka3
+let pembagianAngka = angka1/angka2/angka3
+let modAngka = angka1%angka2%angka3
+
+
+console.log ("penjumlahan= "+penjumlahanAngka)
+console.log ("Pengurangan= "+penguranganAngka)
+console.log ("Perkalian= "+perkalianAngka)
+console.log ("Pembagian= "+ pembagianAngka)
+console.log ("modulus= "+modAngka)
+
+
+console.log ("Perbandingan")
+
+let kurangDari1= 1<2
+let kurangDari2= 3<2
+console.log ("< "+kurangDari1)
+console.log ("< "+kurangDari2)
+
+let lebihDari1 = 3 > 4
+let lebihDari2 = 5 > 4
+console.log ("> "+lebihDari1)
+console.log ("> "+lebihDari2)
+
+let samaDengan1 = 1 == 2
+let samaDengan2 = "1" == 1
+console.log ("=="+samaDengan1)
+console.log ("=="+samaDengan2)
+
+let lebihBesarSamaDengan1 =2 >=3
+let lebihBesarSamaDengan2 = 4 >=5
+
+console.log (">="+lebihBesarSamaDengan1)
+console.log (">="+lebihBesarSamaDengan2)
+
+let lebihKecilSamaDengan1 =3 <=4
+let lebihKecilSamaDengan2 =3 <=7
+console.log ("<="+lebihKecilSamaDengan1)
+console.log ("<="+lebihKecilSamaDengan2)
+
+let samaDengan3x1 = 1 === 1
+let samaDengan3x2 = 1 === "1"
+console.log ("==="+samaDengan3x1)
+console.log ("==="+samaDengan3x2)
+
+let tidakSamaDengan1 = 1 != 1
+let tidakSamaDengan2 = 2 != 4
+console.log ("!="+tidakSamaDengan1)
+console.log ("!="+tidakSamaDengan2)
+
+let tidakSamaDengan2x1 = 1 !== 1
+let tidakSamaDengan2x2 = 1 !== "1"
+console.log ("!=="+tidakSamaDengan2x1)
+console.log ("!=="+tidakSamaDengan2x2)
