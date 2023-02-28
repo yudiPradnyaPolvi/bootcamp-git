0ef0fd0c1eeeb24eab85365f6a440d8686de75f7
yudiPradnyaPolvi <pradnyakemenuh@gmail.com>
2023-02-28 21:47:59 +0800

    inisiasi project

---
+++ helo.js
@@ -0,0 +1,61 @@
+console.log("hello world")
+console.log("hello world")
+
+// variable
+var umur = 10
+console.log(umur)
+let namaPanggilan = "polvi" 
+namaPanggilan = "yudi"
+console.log (namaPanggilan)
+const a = 10
+//a = 4
+console.log(a)
+
+//Tipe data
+console.log('tipe string')
+const nama = 'yudi'
+let usia = 10
+console.log ("nama" + nama , "usia"+ usia )
+const namaBelakang ="pradnya"
+const namaLengkap=`I ${nama} ${namaBelakang}`
+console.log (namaLengkap)
+
+// operator 
+console.log ('Operator Aritmatika')
+
+const angka1= 51
+const angka2 = 2
+const penjumlahanAngka = angka1+angka2
+const penguranganAngka = angka1-angka2
+const perkalianAngka = angka1*angka2
+const pembagianAngka = angka1/angka2
+const perpangkatanAngka = angka1**angka2
+const sisaHasilBagiAngka = angka1%angka2
+
+console.log ("penjumlahan= "+penjumlahanAngka)
+console.log ("Pengurangan= "+penguranganAngka)
+console.log ("Perkalian= "+perkalianAngka)
+console.log ("Pembagian= "+ pembagianAngka)
+console.log ("Perpangkatan= "+perpangkatanAngka)
+console.log ("Sisa Hasil Bagi= "+sisaHasilBagiAngka)
+
+console.log("Operator Perbandingan")
+angka3 = 2
+angka4 = 3
+const kurangDari= angka3<angka4
+const lebihDari = angka3 > angka4
+const samaDengan = angka3 == angka4
+const lebihBesarSamaDengan =angka3 >=angka4
+const lebihKecilSamaDengan =angka3 <=angka4
+const samaDengan3 = angka3 === angka4
+const tidakSamaDengan = angka3 != angka4
+
+console.log("Apakah angka 3 lebih besar dari angka 4 = " + lebihDari )
+console.log("Apakah angka 3 kurang dari angka 4 = " + kurangDari )
+console.log("Apakah angka 3 sama dengan angka 4 = " + samaDengan)
+console.log("Apakah angka 3 lebih besar  dari sama dengan angka 4 = " + lebihBesarSamaDengan)
+console.log("Apakah angka 3 kurang dari angka 4 = " + lebihKecilSamaDengan )
+console.log("Apakah angka 3 sama dengan termasuk tipedatanya angka 4 = " + samaDengan3 )
+console.log("Apakah angka 3 tidak sama dengan angka 4 = " + tidakSamaDengan )
+
+
