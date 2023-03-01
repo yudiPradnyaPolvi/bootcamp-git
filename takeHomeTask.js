20be78c7108f0a96ef0738c88be8924a858c2064
yudiPradnyaPolvi <pradnyakemenuh@gmail.com>
2023-03-01 23:49:55 +0800

    inisiasi project

---
+++ takeHomeTask.js
@@ -0,0 +1,234 @@
+console.log("20 Variable dengan 5 angka")
+
+
+let angka1 = 10
+let angka2 = 30
+let angka3 = 40
+let angka4 = 60
+let angka5 = 70
+
+let hasil1 = angka1+angka2-angka3+angka4-angka5
+let hasil2 = angka1-angka2+angka3-angka4+angka5
+let hasil3 = angka1-angka2*angka3+angka4/angka5
+let hasil4 = angka1%angka2*angka3-angka4+angka5
+let hasil5 = angka1+angka2+angka3/angka4*angka5
+let hasil6 = angka1*angka2-angka3+angka4+angka5
+let hasil7 = angka1**angka2/angka3%angka4-angka5
+let hasil8 = angka1+angka5-angka2+angka3/angka4 
+let hasil9 = angka1-angka2%angka4/angka3*angka5
+let hasil10 = angka1+angka2*angka3+angka4-angka5
+let hasil11 = angka3+angka4+angka5+angka1+angka2
+let hasil12 = angka3-angka4-angka5+angka1-angka2
+let hasil13 = angka4*angka5*angka1/angka2*angka3
+let hasil14 = angka3/angka4/angka1*angka5+angka2
+let hasil15 = angka2%angka3*angka1+angka4/angka5
+let hasil16 = angka1**angka3/angka2-angka4%angka5 
+let hasil17 = angka4/angka3*angka2-angka5+angka1
+let hasil18 = angka5*angka2/angka4-angka3+angka5
+let hasil19 = angka1+angka2*angka3/angka5%angka4
+let hasil20 = angka2-angka3+angka4-angka1*angka5
+
+console.log ("Tampilkan Hasil1 = " + hasil1)
+console.log ("Tampilkan Hasil2 = " + hasil2)
+console.log ("Tampilkan Hasil3 = " + hasil3)
+console.log ("Tampilkan Hasil4 = " + hasil4)
+console.log ("Tampilkan Hasil5 = " + hasil5)
+console.log ("Tampilkan Hasil6 = " + hasil6)
+console.log ("Tampilkan Hasil7 = " + hasil7)
+console.log ("Tampilkan Hasil8 = " + hasil8)
+console.log ("Tampilkan Hasil9 = " + hasil9)
+console.log ("Tampilkan Hasil10 = " + hasil10)
+console.log ("Tampilkan Hasil11 = " + hasil11)
+console.log ("Tampilkan Hasil12 = " + hasil12)
+console.log ("Tampilkan Hasil13 = " + hasil13)
+console.log ("Tampilkan Hasil14 = " + hasil14)
+console.log ("Tampilkan Hasil15 = " + hasil15)
+console.log ("Tampilkan Hasil16 = " + hasil16)
+console.log ("Tampilkan Hasil17 = " + hasil17)
+console.log ("Tampilkan Hasil18 = " + hasil18)
+console.log ("Tampilkan Hasil19 = " + hasil19)
+console.log ("Tampilan Hasil20 = " + hasil20)
+
+
+console.log("Perbandingan")
+
+
+let kurangDari1= 1<2
+let kurangDari2= 2<2
+let kurangDari3= 4<2
+let kurangDari4= 5<3
+let kurangDari5= 1<1
+let kurangDari6= 3<4
+let kurangDari7= 5<2
+let kurangDari8= 6<3
+let kurangDari9= 1<5
+let kurangDari10= 2<1
+console.log ("< "+kurangDari1)
+console.log ("< "+kurangDari2)
+console.log ("< "+kurangDari3)
+console.log ("< "+kurangDari4)
+console.log ("< "+kurangDari5)
+console.log ("< "+kurangDari6)
+console.log ("< "+kurangDari7)
+console.log ("< "+kurangDari8)
+console.log ("< "+kurangDari9)
+console.log ("< "+kurangDari10)
+
+let lebihDari1 = 2 > 4
+let lebihDari2 = 6 > 4
+let lebihDari3 = 7 > 3
+let lebihDari4 = 2 > 2
+let lebihDari5 = 2 > 1
+let lebihDari6 = 5 > 5
+let lebihDari7 = 6 > 4
+let lebihDari8 = 7 > 1
+let lebihDari9 = 3 > 2
+let lebihDari10 = 7 > 4
+
+console.log ("> "+lebihDari1)
+console.log ("> "+lebihDari2)
+console.log ("> "+lebihDari3)
+console.log ("> "+lebihDari4)
+console.log ("> "+lebihDari5)
+console.log ("> "+lebihDari6)
+console.log ("> "+lebihDari7)
+console.log ("> "+lebihDari8)
+console.log ("> "+lebihDari9)
+console.log ("> "+lebihDari10)
+
+
+let samaDengan1 = 2 == 9
+let samaDengan2 = "1" == 1
+let samaDengan3 = 3 == 2
+let samaDengan4 = "2" == "2"
+let samaDengan5 = 3 == 2
+let samaDengan6 = "1" == 4
+let samaDengan7 = 5 == 2
+let samaDengan8 = "6" == 1
+let samaDengan9 = 1 == 7
+let samaDengan10 = "8" == 3
+
+console.log ("=="+samaDengan1)
+console.log ("=="+samaDengan2)
+console.log ("=="+samaDengan3)
+console.log ("=="+samaDengan4)
+console.log ("=="+samaDengan5)
+console.log ("=="+samaDengan6)
+console.log ("=="+samaDengan7)
+console.log ("=="+samaDengan8)
+console.log ("=="+samaDengan9)
+console.log ("=="+samaDengan10)
+
+
+let lebihBesarSamaDengan1 = 3 >= 3
+let lebihBesarSamaDengan2 = 6 >= 2
+let lebihBesarSamaDengan3 = 7 >= 5
+let lebihBesarSamaDengan4 = 5 >= 7
+let lebihBesarSamaDengan5 = 6 >= 8
+let lebihBesarSamaDengan6 = 7 >= 9
+let lebihBesarSamaDengan7 = 2 >= 5
+let lebihBesarSamaDengan8 = 4 >= 2
+let lebihBesarSamaDengan9 = 5 >= 3
+let lebihBesarSamaDengan10 = 8 >=6
+
+console.log (">="+lebihBesarSamaDengan1)
+console.log (">="+lebihBesarSamaDengan2)
+console.log (">="+lebihBesarSamaDengan3)
+console.log (">="+lebihBesarSamaDengan4)
+console.log (">="+lebihBesarSamaDengan5)
+console.log (">="+lebihBesarSamaDengan6)
+console.log (">="+lebihBesarSamaDengan7)
+console.log (">="+lebihBesarSamaDengan8)
+console.log (">="+lebihBesarSamaDengan9)
+console.log (">="+lebihBesarSamaDengan10)
+
+
+let lebihKecilSamaDengan1 = 1<=6
+let lebihKecilSamaDengan2 = 2<=7
+let lebihKecilSamaDengan3 = 5<=3
+let lebihKecilSamaDengan4 = 6<=5
+let lebihKecilSamaDengan5 = 7<=6
+let lebihKecilSamaDengan6 = 2<=3
+let lebihKecilSamaDengan7 = 6<=7
+let lebihKecilSamaDengan8 = 5<=5
+let lebihKecilSamaDengan9 = 7<=3
+let lebihKecilSamaDengan10 = 8<=9
+
+console.log ("<="+lebihKecilSamaDengan1)
+console.log ("<="+lebihKecilSamaDengan2)
+console.log ("<="+lebihKecilSamaDengan3)
+console.log ("<="+lebihKecilSamaDengan4)
+console.log ("<="+lebihKecilSamaDengan5)
+console.log ("<="+lebihKecilSamaDengan6)
+console.log ("<="+lebihKecilSamaDengan7)
+console.log ("<="+lebihKecilSamaDengan8)
+console.log ("<="+lebihKecilSamaDengan9)
+console.log ("<="+lebihKecilSamaDengan10)
+
+let samaDengan3x1 = 2 === 2
+let samaDengan3x2 = "1" === "1"
+let samaDengan3x3 = 2 === 1
+let samaDengan3x4 = 3 === "3"
+let samaDengan3x5 = 4 === 4
+let samaDengan3x6 = 5 === "2"
+let samaDengan3x7 = 2 === 4
+let samaDengan3x8 = 5 === "3"
+let samaDengan3x9 = 6 === 5
+let samaDengan3x10 = 4 === "2"
+
+console.log ("==="+samaDengan3x1)
+console.log ("==="+samaDengan3x2)
+console.log ("==="+samaDengan3x3)
+console.log ("==="+samaDengan3x4)
+console.log ("==="+samaDengan3x5)
+console.log ("==="+samaDengan3x6)
+console.log ("==="+samaDengan3x7)
+console.log ("==="+samaDengan3x8)
+console.log ("==="+samaDengan3x9)
+console.log ("==="+samaDengan3x10)
+
+
+let tidakSamaDengan1 = 1 != 3
+let tidakSamaDengan2 = 5 != 6
+let tidakSamaDengan3 = 3 != 2
+let tidakSamaDengan4 = "4" != 4
+let tidakSamaDengan5 = 2 != 5
+let tidakSamaDengan6 = 4 != 3
+let tidakSamaDengan7 = 6 != 6
+let tidakSamaDengan8 = 3 != 7
+let tidakSamaDengan9 = 7 != 2
+let tidakSamaDengan10 = 8 != "8"
+
+console.log ("!="+tidakSamaDengan1)
+console.log ("!="+tidakSamaDengan2)
+console.log ("!="+tidakSamaDengan3)
+console.log ("!="+tidakSamaDengan4)
+console.log ("!="+tidakSamaDengan5)
+console.log ("!="+tidakSamaDengan6)
+console.log ("!="+tidakSamaDengan7)
+console.log ("!="+tidakSamaDengan8)
+console.log ("!="+tidakSamaDengan9)
+console.log ("!="+tidakSamaDengan10)
+
+
+let tidakSamaDengan2x1 = 2 !== 1
+let tidakSamaDengan2x2 = 3 !== "3"
+let tidakSamaDengan2x3 = 4 !== 4
+let tidakSamaDengan2x4 = 8 !== "2"
+let tidakSamaDengan2x5 = 6 !== 5
+let tidakSamaDengan2x6 = 2 !== "6"
+let tidakSamaDengan2x7 = 3 !== 3
+let tidakSamaDengan2x8 = 6 !== "6"
+let tidakSamaDengan2x9 = 7 !== 5
+let tidakSamaDengan2x10 = 8 !== "6"
+
+console.log ("!=="+tidakSamaDengan2x1)
+console.log ("!=="+tidakSamaDengan2x2)
+console.log ("!=="+tidakSamaDengan2x3)
+console.log ("!=="+tidakSamaDengan2x4)
+console.log ("!=="+tidakSamaDengan2x5)
+console.log ("!=="+tidakSamaDengan2x6)
+console.log ("!=="+tidakSamaDengan2x7)
+console.log ("!=="+tidakSamaDengan2x8)
+console.log ("!=="+tidakSamaDengan2x9)
+console.log ("!=="+tidakSamaDengan2x10)
